aa48b1a1f09271b9528e103e60d26b49461cd787
atarubift <atarubift@gmail.com>
2022-11-08 14:34:58 +0900

    add: loop method

--- mine/mine_calc.py
+++ mine/mine_calc.py
@@ -1,4 +1,7 @@
-blocks = input("必要ブロック数：")
+blocks = ""
 
-print('%s Stack + %s'% (int(blocks) // 64, int(blocks) % 64))
+while blocks != "0" :
+    blocks = input("必要ブロック数：")
+    num = int(blocks)
+    print('%s Stack + %s'% (num // 64, num % 64))
 
